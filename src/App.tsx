18ca55019ad78c0001f08346cbbf2287cf005ad4
joaosilva-web <joaogustavosilva2001@gmail.com>
2025-02-15 17:48:17 -0300

    Create TransactionsContainer
    Now is possible to see a table with all transactions

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import { Container } from "@chakra-ui/react";
 import { Header } from "./components/Header";
 import { StatsContainer } from "./components/StatsContainer";
+import { TransactionsContainer } from "./components/TransactionsContainer";
 
 function App() {
   return (
     <Container maxW="100%" p="0" bg="background" minH="100vh">
       <Header/>
       <StatsContainer/>
+      <TransactionsContainer/>
     </Container>
   );
 }
