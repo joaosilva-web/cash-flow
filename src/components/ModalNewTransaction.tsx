614d237b4bc236bce04082faa0b4c317ca5bb6f2
joaosilva-web <joaogustavosilva2001@gmail.com>
2025-02-16 22:56:56 -0300

    Create a component to ModaNweTransaction
    Now we have a exclusive file for the ModalNewTransaction, this mantain our code more legible

---
+++ src/components/ModalNewTransaction.tsx
@@ -0,0 +1,118 @@
+import {
+    Button,
+    ButtonGroup,
+    FormControl,
+    Heading,
+    Icon,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Text,
+    VStack,
+    Flex,
+  } from "@chakra-ui/react";
+  import { ArrowCircleDown, ArrowCircleUp } from "@phosphor-icons/react";
+  import { useReward } from "react-rewards";
+  
+  interface IModalNewTransactionProps {
+    isModalOpen: boolean;
+    onModalClose: () => void;
+  }
+  
+  export function ModalNewTransaction({
+    isModalOpen,
+    onModalClose,
+  }: IModalNewTransactionProps) {
+    const { reward: rewardInflow } = useReward("rewardId", "emoji", {
+      emoji: ["🤑", "💲", "📈"],
+      lifetime: 100,
+    });
+    const { reward: rewardOutflow } = useReward("rewardOutflow", "emoji", {
+      emoji: ["💸", "😭", "📉"],
+      lifetime: 100,
+    });
+  
+    return (
+      <Modal isOpen={isModalOpen} onClose={onModalClose}>
+        <ModalOverlay />
+        <ModalContent w="576px">
+          <ModalCloseButton />
+          <ModalHeader paddingTop="4rem" paddingBottom="2rem">
+            <Heading as="h2" color="title" fontSize="1.25rem">
+              Register Transaction
+            </Heading>
+          </ModalHeader>
+          <ModalBody>
+            <FormControl>
+              <VStack spacing="1rem">
+                <Input h="64px" placeholder="Title" type="text" />
+                <Input h="64px" placeholder="Value" type="text" />
+                <ButtonGroup w="100%">
+                  <Button
+                    bg="white"
+                    border="2px solid"
+                    borderColor="gray.300"
+                    flex="1"
+                    h="64px"
+                    _hover={{ borderColor: "green" }}
+                    _focus={{ borderColor: "green", backgroundColor: "transparent" }}
+                    onClick={rewardInflow}
+                  >
+                    <span id="rewardId" />
+                    <Flex align="center" gap="0.5rem">
+                      <Icon as={ArrowCircleUp} color="green" h="24px" w="24px" />
+                      <Text>Inflow</Text>
+                    </Flex>
+                  </Button>
+                  <Button
+                    border="2px solid"
+                    bg="white"
+                    borderColor="gray.300"
+                    flex="1"
+                    h="64px"
+                    _hover={{ borderColor: "red" }}
+                    _focus={{ borderColor: "red" }}
+                    onClick={rewardOutflow}
+                  >
+                    <span id="rewardOutflow" />
+                    <Flex align="center" gap="0.5rem">
+                      <Icon as={ArrowCircleDown} color="red" h="24px" w="24px" />
+                      <Text>Outflow</Text>
+                    </Flex>
+                  </Button>
+                </ButtonGroup>
+                <Input type="text" h="64px" placeholder="Category" />
+                <Button
+                  bg="green"
+                  color="white"
+                  height="64px"
+                  marginTop="1.25rem"
+                  marginBottom="1rem"
+                  type="submit"
+                  w="100%"
+                  _hover={{
+                    backgroundColor: "green.900",
+                    transform: "scale(1.01)",
+                  }}
+                  _focus={{ backgroundColor: "green" }}
+                >
+                  Register
+                </Button>
+              </VStack>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button variant="outline" onClick={onModalClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    );
+  }
+  
