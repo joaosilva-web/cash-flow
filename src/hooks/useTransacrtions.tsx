45467bd25d09fda2b0700129757fc10145a3dea3
joaosilva-web <joaogustavosilva2001@gmail.com>
2025-02-22 14:45:38 -0300

    create useTransactions provider and context

---
+++ src/hooks/useTransacrtions.tsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState } from "react";
+
+
+export interface IUserTransactions {
+    id: string;
+    title: string;
+    value: number;
+    category: string;
+    date: Date;
+    type: "inflow" | "outflow";
+  }
+
+  interface TransactionsContextType {
+    transactions: IUserTransactions[];
+    setTransactions: React.Dispatch<React.SetStateAction<IUserTransactions[]>>;
+  }
+
+  const TransactionsContext = createContext<TransactionsContextType | undefined>(undefined);
+
+  //Creating Provider
+  export function TransactionsProvider({children}: {children: React.ReactNode}){
+    const [transactions, setTransactions] = useState<IUserTransactions[]>([]);
+
+    return (
+        <TransactionsContext.Provider value={{transactions, setTransactions}}>
+            {children}
+        </TransactionsContext.Provider>
+    )
+  }
+
+  //Creating Hook
+  export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    if(!context){
+        throw new Error("useTransactions must be used within TransactionsProvider");
+    }
+
+    return context;
+  }
